35f95865fc06b95a096d1a19587f880a7df884af
SangHyung-Jung <cukirhkwk@hanyang.ac.kr>
2021-01-19 22:50:22 +0900

    final4

--- helper/db_helper.py
+++ helper/db_helper.py
@@ -3,27 +3,37 @@ from crawling import REALTIME
 
 class DB_HELPER():
     def __init__(self, host = '13.209.6.72', user = 'test', passwd = 'qkqhdi12', db = 'testdb'):
-        self.connection = pymysql.connect(
-            host = host,
-            user = user,
-            passwd = passwd,
-            db= db,
-            charset = 'utf8'
-        )
+        self.host = host
+        self.user = user
+        self.passwd = passwd
+        self.db = db
         self.data = REALTIME()
 
-
     def read_tables(self, dbname='testdb', table_name = 'soo2'):
-        cursor = self.connection.cursor(pymysql.cursors.DictCursor)
+        connection = pymysql.connect(
+                    host = self.host,
+                    user = self.user,
+                    passwd = self.passwd,
+                    db= self.db,
+                    charset = 'utf8'
+                )        
+        cursor = connection.cursor(pymysql.cursors.DictCursor)
         sql = "SELECT * FROM {};".format(table_name)
         cursor.execute(sql)
         result = cursor.fetchall()
+        self.connection.close()
 
         return result
     
     def update_tables(self, dbname='testdb', table_name = 'news'):
-
-        cursor = self.connection.cursor(pymysql.cursors.DictCursor)
+        connection = pymysql.connect(
+                    host = self.host,
+                    user = self.user,
+                    passwd = self.passwd,
+                    db= self.db,
+                    charset = 'utf8'
+                )
+        cursor = connection.cursor(pymysql.cursors.DictCursor)
         sql = "DELETE FROM soo2;"
         cursor.execute(sql)
 
@@ -33,5 +43,4 @@ class DB_HELPER():
         cursor.executemany(sql, val)
 
         self.connection.commit()
-
         self.connection.close()
