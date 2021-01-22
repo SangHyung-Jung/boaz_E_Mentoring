c70e584c25f20a01046bbad03205672847016d16
JungSangHyung <52977787+JungSangHyung@users.noreply.github.com>
2021-01-23 01:48:05 +0900

    rename

--- wsgi.py
+++ wsgi.py
@@ -2,6 +2,7 @@ import sys
 import site
 
 site.addsitedir('/var/www/flaskapp/lib/python3.6/site-packages')
+site.addsitedir('/home/ec2-user/jsh/env/lib/python3.6/site-packages')
 
 sys.path.insert(0, '/var/www/flaskapp')
 
